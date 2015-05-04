39b7fe01769e77f5d9132a435625259e2b15a4e9
pytlab <shaozhengjiang@gmail.com>
2015-05-04 16:25:36 +0800

    Add exponential to term
    添加了化学计量书不为1的情况下平衡常数表达式中每一项的指数也不为1的情况。

--- pynetics/solvers/quasi_equilibrium_solver.py
+++ pynetics/solvers/quasi_equilibrium_solver.py
@@ -19,6 +19,22 @@ class QuasiEquilibriumSolver(SolverBase):
 
         self.represented_thetas = []  # thetas that has been represented by theta*
 
+    def get_tof_sym(self):
+        waiting_room = []
+        for rxn_idx, rxn_list in enumerate(self.rxns_list):
+            #get adsorbate name that will be represented
+            target_adsorbate = self.check_repr(rxn_list)
+            #get target adsorbate theta symbol
+            theta_target = self.extract_symbol(target_adsorbate, 'ads_cvg')
+            #create a free site theta symbol used in all rxns
+            theta_f = sym.Symbol('theta_f', is_positive=True, is_real=True)
+
+            if target_adsorbate:
+                pass
+
+            else:  # throw it to waiting room
+                waiting_room.append(rxn_list)
+
     def represent(self, rxn_list, target_adsorbate, theta_f):
         """
         Expect a rxn_list which can be represented by theta_f,
@@ -52,6 +68,8 @@ class QuasiEquilibriumSolver(SolverBase):
                         theta_f_loc = 'left'
                     else:
                         theta_f_loc = 'right'
+                    #add exponential
+                    theta_f_term = theta_f**stoichiometry
                 #target species theta
                 elif species_name == target_adsorbate:
                     # get location of theta_target_ads
@@ -59,36 +77,42 @@ class QuasiEquilibriumSolver(SolverBase):
                         theta_t_loc = 'left'
                     else:
                         theta_t_loc = 'right'
+                    #get theta_t term exponential
+                    theta_t_exp = stoichiometry
                 #represented adsorbate
                 elif species_name in self._owner.adsorbate_names:
                     #extract symbol of the adsorbate
                     theta_sym = self.extract_symbol(species_name, 'ads_cvg')
+                    #add exponential
+                    theta_sym_term = theta_sym**stoichiometry
                     #get location of theta represented already
                     if state_idx == 0:
-                        left_syms.append(theta_sym)
+                        left_syms.append(theta_sym_term)
                     else:
-                        right_syms.append(theta_sym)
+                        right_syms.append(theta_sym_term)
                 #gas pressure
                 elif species_name in self._owner.gas_names:
                     p_sym = self.extract_symbol(species_name, 'pressure')
+                    #add exponential
+                    p_sym_term = p_sym**stoichiometry
                     if state_idx == 0:
-                        left_syms.append(p_sym)
+                        left_syms.append(p_sym_term)
                     else:
-                        right_syms.append(p_sym)
+                        right_syms.append(p_sym_term)
 
         #use theta_f to represent theta_target_adsorbate
         '''
-        ---------------------------------------------------------------------
-        |theta_* location | theta_t_loc | theta_target_adsorbate expression |
-        ---------------------------------------------------------------------
-        |    left         |    left     |            R/(K*L*theta_f)        |
-        ---------------------------------------------------------------------
-        |    left         |    right    |            K*L*theta_f/R          |
-        ---------------------------------------------------------------------
-        |    right        |    left     |            R*theta_f/(L*K)        |
-        ---------------------------------------------------------------------
-        |    right        |    right    |            K*L/(R*theta_f)        |
-        ---------------------------------------------------------------------
+        --------------------------------------------------------------------------
+        |theta_* location | theta_t_loc | theta_target_adsorbate expression      |
+        --------------------------------------------------------------------------
+        |    left         |    left     |            R/(K*L*theta_f_term)        |
+        --------------------------------------------------------------------------
+        |    left         |    right    |            K*L*theta_f_term/R          |
+        --------------------------------------------------------------------------
+        |    right        |    left     |            R*theta_f_term/(L*K)        |
+        --------------------------------------------------------------------------
+        |    right        |    right    |            K*L/(R*theta_f_term)        |
+        --------------------------------------------------------------------------
         '''
         def get_multi_sym_list(syms_list):
             multi_sym = 1
@@ -98,15 +122,16 @@ class QuasiEquilibriumSolver(SolverBase):
 
         L = get_multi_sym_list(left_syms)  # left multipled symbols
         R = get_multi_sym_list(right_syms)  # right multipled symbols
+        total_exp = 1.0/theta_t_exp
 
         if theta_f_loc == 'left' and theta_t_loc == 'left':
-            theta_t = R/(K*L*theta_f)
+            theta_t = (R/(K*L*theta_f_term))**total_exp
         elif theta_f_loc == 'left' and theta_t_loc == 'right':
-            theta_t = K*L*theta_f/R
+            theta_t = (K*L*theta_f_term/R)**total_exp
         elif theta_f_loc == 'right' and theta_t_loc == 'left':
-            theta_t = R*theta_f/(L*K)
+            theta_t = (R*theta_f_term/(L*K))**total_exp
         elif theta_f_loc == 'right' and theta_t_loc == 'right':
-            theta_t = K*L/(R*theta_f)
+            theta_t = (K*L/(R*theta_f_term))**total_exp
 
         return theta_t
 
