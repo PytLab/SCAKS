e896153c21eb9b1f792c0505af0eb2a48222a4f8
pytlab <shaozhengjiang@gmail.com>
2015-06-04 19:59:41 +0800

    Change parser

--- pynetics/model.py
+++ pynetics/model.py
@@ -119,8 +119,8 @@ class KineticModel(object):
         defaults = dict(
             data_file='data.pkl',
             decimal_precision=100,
-            #parser='CsvParser',
-            parser='RelativeEnergyParser',
+            parser='CsvParser',
+            #parser='RelativeEnergyParser',
             table_maker='CsvMaker',
             solver='SteadyStateSolver',
             #solver='QuasiEquilibriumSolver',
