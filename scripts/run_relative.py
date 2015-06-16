7602ffc0ddff31a28d10c7ecfba3e95b71f85601
pytlab <shaozhengjiang@gmail.com>
2015-06-16 10:10:02 +0800

    Use archived cvg as initial guess

--- scripts/run_relative.py
+++ scripts/run_relative.py
@@ -1,8 +1,13 @@
 import sys
+import os
+import cPickle as cpkl
 
 sys.path.append('D:\Dropbox\Code\Python\kinetic\Pynetics')
 from pynetics import model
 
+if os.path.exists('event.log'):  # del old log file
+    os.remove('event.log')
+    print "delete 'event.log'."
 #create micro kinetic model instance
 m = model.KineticModel(setup_file='furfural.mkm')
 
@@ -10,7 +15,7 @@ m.parser.parse_data()  # parse data from rel_energy.py
 m.solver.get_data_dict()  # solver get data from parser
 m.solver.get_rate_constants()
 m.solver.get_rate_expressions(m.solver.rxns_list)
-
+'''
 for i, rxn_equation in enumerate(m.rxn_expressions):
     print 'Plotting diagram '+str(i)+'...'
     m.plotter.plot_single_energy_diagram(rxn_equation, show_mode='save')
@@ -19,13 +24,26 @@ for i, rxn_equation in enumerate(m.rxn_expressions):
 print "Plotting multi_energy_diagram..."
 m.plotter.plot_multi_energy_diagram(m.rxn_expressions, show_mode='save')
 print 'Ok.\n'
+'''
 
-if len(sys.argv) > 1 and '-c' == sys.argv[1]:
+if len(sys.argv) > 1 and 'c' in sys.argv[1]:
     print 'Correct free energies...'
     m.solver.correct_energies()
     print 'Ok.'
 
-b_cvg = m.solver.boltzmann_coverages()
-#init_cvg = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-cvg = m.solver.get_steady_state_cvgs(b_cvg)
-print m.adsorbate_names
+#set initial guess(initial coverage)
+#init_cvg = (0.0, 0.0, 0.0, 0.0, 0.0, 0.00, 0.0, 0.0, 0.99, 0.0, 0.0, 0.0, 0.0, 0.0)
+#if there is converged coverage, use it as initial guess
+if os.path.exists("./data.pkl"):
+    with open('data.pkl', 'rb') as f:
+        data = cpkl.load(f)
+    if 'steady_state_coverage' in data:
+        init_cvg = data['steady_state_coverage']
+    elif "initial_guess" in data:
+        init_cvg = data['initial_guess']
+    else:
+        init_cvg = m.solver.boltzmann_coverages()
+else:
+    init_cvg = m.solver.boltzmann_coverages()
+
+cvg = m.solver.get_steady_state_cvgs(init_cvg)
