cab5761928697049f1e2b2703c8209133747fb7c
pytlab <shaozhengjiang@gmail.com>
2015-06-24 10:11:42 +0800

    Use steady state cvg as initial guess

--- pynetics/solvers/solver_base.py
+++ pynetics/solvers/solver_base.py
@@ -395,7 +395,10 @@ class SolverBase(ModelShell):
         #get net rates about new Gs
         self.get_rate_constants()
         #get initial guess
-        init_guess = self.initial_guess
+        if self._coverage:
+            init_guess = self._coverage
+        else:
+            init_guess = self.initial_guess
         steady_state_cvg = self.get_steady_state_cvgs(init_guess)
         #check whether solver has rate_expressions
         if not hasattr(self, 'rate_expressions'):  # if not, get it
