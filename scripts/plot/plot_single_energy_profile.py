f180d78f4d94af58be81d7ebf99498a42fc4f0b4
pytlab <shaozhengjiang@gmail.com>
2015-06-26 10:08:53 +0800

    Add multi_diagram plotting

--- scripts/plot/plot_single_energy_profile.py
+++ scripts/plot/plot_single_energy_profile.py
@@ -7,8 +7,14 @@ if len(rxn_equations) != len(energy_tuples):
     raise ValueError("lengths of rxn_equations and energy_tuples " +
                      "are different.")
 
+#plot single diagrams
 for idx, args in enumerate(zip(energy_tuples, rxn_equations)):
     fname = str(idx).zfill(2)
     print "Plotting diagram " + fname + "..."
     plot_single_energy_diagram(*args, show_mode='save', fname=fname)
     print "Ok."
+
+#plot multi-diagram
+print "Plotting multi-diagram..."
+plot_multi_energy_diagram(rxn_equations, energy_tuples, show_mode='save')
+print "Ok."
