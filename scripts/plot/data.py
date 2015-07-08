1701b0a8a1707d454c0b64f27dfb4a95c21c2b56
PytLab <shaozhengjiang@gmail.com>
2015-07-08 20:20:05 +0800

    Add new template

--- scripts/plot/data.py
+++ scripts/plot/data.py
@@ -1,7 +1,7 @@
 '''
     Data for energy profile plotting.
 '''
-
+'''
 #elementary reaction equations
 rxn_equations = [
     'HCOOH_g + 2*_s <-> HCOO-H_s + *_s -> HCOO_s + H_s',
@@ -19,3 +19,25 @@ energy_tuples = [
     (0.0, 4.0),
     (3.0, 4.7, 0.7),
 ]
+'''
+'''
+rxn_equations = [
+    'HCOOH_g + 2*_s <-> HCOO-H_s + *_s -> HCOO_s + H_s',
+    'HCOO_s + *_s -> COO_s + H_s',
+    '2H_s -> H2_g + 2*_s'
+]
+
+#relative energies
+energy_tuples = [
+    # IS,  TS,  FS
+    #--------------
+    (0.0, 0.12, -0.26),
+    (-0.26, -0.56),
+    (-0.56, -0.42),
+]
+'''
+
+rxn_equation = 'HCOOH_g + 2*_s <-> HCOO-H_s + *_s -> HCOO_s + H_s'
+
+#relative energies
+energy_tuple = (0.0, 0.33, -0.26)
