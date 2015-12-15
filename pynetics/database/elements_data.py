6cd1e546078fcd2c419d02d6642408f0e59c5c36
PytLab <shaozhengjiang@gmail.com>
2015-12-15 16:13:14 +0800

    initial commit

---
+++ pynetics/database/elements_data.py
@@ -0,0 +1,15 @@
+'''
+    Module to store chemical elements related data.
+'''
+C12 = 1.99264648e-26  # mass of C12 (kg/atom)
+amu = 1.66053904e-27  # atomic mass unit (kg)
+
+chem_element = {}
+chem_element['H'] = dict(index=1, mass=1.00794)
+chem_element['C'] = dict(index=6, mass=12.0107)
+chem_element['N'] = dict(index=7, mass=14.0067)
+chem_element['O'] = dict(index=8, mass=15.9994)
+chem_element['F'] = dict(index=9, mass=18.9984032)
+chem_element['Ne'] = dict(index=10, mass=20.1797)
+chem_element['S'] = dict(index=16, mass=32.065)
+chem_element['Cl'] = dict(index=17, mass=35.453)
