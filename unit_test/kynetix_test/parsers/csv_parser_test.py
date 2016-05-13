279e85ecb5b9b9445223d24e255d3479c88546be
PytLab <shaozhengjiang@gmail.com>
2016-05-13 16:33:57 +0800

    Add relative energies related test.

--- unit_test/kynetix_test/parsers/csv_parser_test.py
+++ unit_test/kynetix_test/parsers/csv_parser_test.py
@@ -21,6 +21,36 @@ class CsvParserTest(unittest.TestCase):
         self.assertTrue(isinstance(parser, CsvParser))
         self.assertEqual(parser.__class__.__base__.__name__, "ParserBase")
 
+    def test_get_relative_energies(self):
+        " Test parsers can calculate reaction barriers correctly. "
+        # Construction.
+        model = KineticModel(setup_file="input_files/csv_parser.mkm",
+                             verbosity=logging.ERROR)
+        parser = model.parser()
+
+        # Before get absolute data.
+        rxn_list = model.elementary_rxns_list()[-1]
+        self.assertRaises(AttributeError, parser.get_relative_energies, rxn_list)
+
+        # Parse absolute data.
+        parser.parse_data()
+        ret_f_barrier, ret_r_barrier, ret_rxn_energy = parser.get_relative_energies(rxn_list)
+        ref_f_barrier, ref_r_barrier, ref_rxn_energy = (1.1, 1.75, -0.6499999999999999)
+
+        self.assertEqual(ref_f_barrier, ret_f_barrier)
+        self.assertEqual(ref_r_barrier, ret_r_barrier)
+        self.assertEqual(ref_rxn_energy, ret_rxn_energy)
+
+        # Check rxn list without TS.
+        rxn_list = model.elementary_rxns_list()[1]
+
+        ret_f_barrier, ret_r_barrier, ret_rxn_energy = parser.get_relative_energies(rxn_list)
+        ref_f_barrier, ref_r_barrier, ref_rxn_energy = (0.0, 2.32, -2.32)
+
+        self.assertEqual(ref_f_barrier, ret_f_barrier)
+        self.assertEqual(ref_r_barrier, ret_r_barrier)
+        self.assertEqual(ref_rxn_energy, ret_rxn_energy)
+
     def test_data_parse(self):
         " Test data in csv file can be read correctly. "
         # Construction.
@@ -80,38 +110,21 @@ class CsvParserTest(unittest.TestCase):
                                     'total': 1.0,
                                     'type': 'site'}}
 
+        # Check model's attr before parse data.
+        self.assertFalse(model.has_absolute_energy())
+        self.assertFalse(model.has_relative_energy())
+        self.assertDictEqual({}, model.relative_energies())
+
         parser.parse_data()
         self.assertDictEqual(ref_species_definitions, model.species_definitions())
+        self.assertTrue(model.has_absolute_energy())
 
-    def test_get_relative_energies(self):
-        " Test parsers can calculate reaction barriers correctly. "
-        # Construction.
-        model = KineticModel(setup_file="input_files/csv_parser.mkm",
-                             verbosity=logging.ERROR)
-        parser = model.parser()
-
-        # Before get absolute data.
-        rxn_list = model.elementary_rxns_list()[-1]
-        self.assertRaises(AttributeError, parser.get_relative_energies, rxn_list)
-
-        # Parse absolute data.
-        parser.parse_data()
-        ret_f_barrier, ret_r_barrier, ret_rxn_energy = parser.get_relative_energies(rxn_list)
-        ref_f_barrier, ref_r_barrier, ref_rxn_energy = (1.1, 1.75, -0.6499999999999999)
-
-        self.assertEqual(ref_f_barrier, ret_f_barrier)
-        self.assertEqual(ref_r_barrier, ret_r_barrier)
-        self.assertEqual(ref_rxn_energy, ret_rxn_energy)
-
-        # Check rxn list without TS.
-        rxn_list = model.elementary_rxns_list()[1]
-
-        ret_f_barrier, ret_r_barrier, ret_rxn_energy = parser.get_relative_energies(rxn_list)
-        ref_f_barrier, ref_r_barrier, ref_rxn_energy = (0.0, 2.32, -2.32)
-
-        self.assertEqual(ref_f_barrier, ret_f_barrier)
-        self.assertEqual(ref_r_barrier, ret_r_barrier)
-        self.assertEqual(ref_rxn_energy, ret_rxn_energy)
+        # Check relative energies.
+        ref_relative_energies = {'Gaf': [0.0, 0.0, 1.1],
+                                 'Gar': [1.1900000000000002, 2.32, 1.75],
+                                 'dG': [-1.1900000000000002, -2.32, -0.6499999999999999]}
+        self.assertDictEqual(ref_relative_energies, model.relative_energies())
+        self.assertTrue(model.has_relative_energy())
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(CsvParserTest)
