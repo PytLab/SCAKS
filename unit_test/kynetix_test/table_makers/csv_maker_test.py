36f360f1a516faa602f7870fe310ccc8e7ce171c
PytLab <shaozhengjiang@gmail.com>
2016-06-19 13:35:20 +0800

    Add new test functions.

--- unit_test/kynetix_test/table_makers/csv_maker_test.py
+++ unit_test/kynetix_test/table_makers/csv_maker_test.py
@@ -21,6 +21,31 @@ class CsvMakerTest(unittest.TestCase):
         # Check.
         self.assertTrue(table_maker, CsvMaker)
 
+    def test_init_table(self):
+        " Test we can initialize table correctly. "
+        model = KineticModel(setup_file="input_files/csv_maker.mkm",
+                             verbosity=logging.WARNING)
+        table_maker = model.table_maker()
+
+        # Init a table.
+        table_maker.init_table()
+
+    def test_get_formation_energy(self):
+        " Test private function __get_formation_energy(). "
+        # Construction.
+        model = KineticModel(setup_file="input_files/csv_maker.mkm",
+                             verbosity=logging.WARNING)
+        table_maker = model.table_maker()
+
+        # Check.
+        ref_e = 0.0
+        ret_e = table_maker._CsvMaker__get_formation_energy("H2_g", -6.759074)
+        self.assertEqual(ref_e, ret_e)
+
+        ref_e = 0.039835999999979776
+        ret_e = table_maker._CsvMaker__get_formation_energy("HCOO-H_s", -207.27)
+        self.assertEqual(ref_e, ret_e)
+
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(CsvMakerTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
