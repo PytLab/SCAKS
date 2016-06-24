22f6773ce5bc0185ea88ba4a4f1c1779cffda0fb
PytLab <shaozhengjiang@gmail.com>
2016-06-24 20:02:09 +0800

    Add CoveragesPluginTest.

---
+++ unit_test/kynetix_test/solvers/kmc_coverages_plugin_test.py
@@ -0,0 +1,32 @@
+import logging
+import re
+import unittest
+
+import numpy as np
+
+from kynetix.model import KineticModel
+from kynetix.solvers import *
+
+
+class KMCCoveragesPluginTest(unittest.TestCase):
+
+    def setUp(self):
+        # Test case setting.
+        self.maxDiff = None
+
+    def test_run_with_coverages(self):
+        " Make sure KMCSolver object can be constructed correctly. "
+        model = KineticModel(setup_file="kmc_inputs/kmc_coverages_plugin.mkm",
+                             verbosity=logging.WARNING)
+        parser = model.parser()
+        parser.parse_data(filename="kmc_inputs/rel_energy.py", relative=True)
+        
+        # Run the model with analysis.
+        model.run_kmc(processes_file="kmc_inputs/kmc_processes.py",
+                      configuration_file="kmc_inputs/kmc_configuration.py",
+                      sitesmap_file="kmc_inputs/kmc_sites.py")
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(KMCCoveragesPluginTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
