a753b984ce6fe6674f2d9612834c631daee69e15
PytLab <shaozhengjiang@gmail.com>
2016-07-02 12:29:09 +0800

    Add exception logging.

--- kynetix/model.py
+++ kynetix/model.py
@@ -239,18 +239,24 @@ class KineticModel(object):
         trajectory_type: The type of trajectory to use, the default type is "lattice", str.
                          "xyz" | "lattice". 
         """
-        parser = self.__parser
+        try:
+            parser = self.__parser
 
-        # Parse processes, configuration, sitesmap.
-        self.__processes = parser.parse_processes(filename=processes_file)
-        self.__configuration = parser.parse_configuration(filename=configuration_file)
-        self.__sitesmap = parser.construct_sitesmap(filename=sitesmap_file)
+            # Parse processes, configuration, sitesmap.
+            self.__processes = parser.parse_processes(filename=processes_file)
+            self.__configuration = parser.parse_configuration(filename=configuration_file)
+            self.__sitesmap = parser.construct_sitesmap(filename=sitesmap_file)
 
-        # Set process reaction mapping.
-        self.__process_mapping = parser.process_mapping()
+            # Set process reaction mapping.
+            self.__process_mapping = parser.process_mapping()
 
-        # Run the lattice model.
-        self.__solver.run(scripting, trajectory_type)
+            # Run the lattice model.
+            self.__solver.run(scripting, trajectory_type)
+        except Exception as e:
+            # Log exception info.
+            msg = "{} exception is catched.".format(type(e).__name__)
+            self.__logger.exception(msg)
+            raise e
 
     def __set_parser(self, parser_name):
         """
