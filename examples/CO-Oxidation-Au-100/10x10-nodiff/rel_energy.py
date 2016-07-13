cc22be6aca0c6b9594bbbfa108712167eb6ab17f
PytLab <shaozhengjiang@gmail.com>
2016-07-13 11:16:39 +0800

    Modify input files.

--- examples/CO-Oxidation-Au-100/10x10-nodiff/rel_energy.py
+++ examples/CO-Oxidation-Au-100/10x10-nodiff/rel_energy.py
@@ -21,3 +21,27 @@ dG.append(-1.02)
 # O2_g + 2*_t -> O2_2t
 Ga.append(0.0)
 dG.append(-0.71)
+
+# O2_2b <-> O-O_2b -> 2O_b
+Ga.append(0.48)
+dG.append(0.47)
+
+# O2_2b + CO_b <-> O-OCO_3b -> O_b + CO2_g + 2*_b
+Ga.append(0.42)
+dG.append(-0.52)
+
+# O_b + CO_t -> CO2_g + *_b + *_t
+Ga.append(0.0)
+dG.append(-3.14)
+
+# CO_b + O_b -> CO2_g + 2*_b
+Ga.append(0.0)
+dG.append(-3.02)
+
+# CO_b + O_b <-> O-CO_2b -> CO2_g + 2*_b
+Ga.append(1.42)
+dG.append(-3.01)
+
+# CO_t + O_b <-> OC-O_b + *_t -> CO2_g + *_t + *_b
+Ga.append(0.77)
+dG.append(-2.05)
