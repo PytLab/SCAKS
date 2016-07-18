b9961c2756818786592e5c5595e0305c4044d356
PytLab <shaozhengjiang@gmail.com>
2016-07-18 23:41:50 +0800

    Add new examples.

---
+++ examples/CO-Oxidation-Pt-111/20x20-nodiff/rel_energy.py
@@ -0,0 +1,16 @@
+# Energy barriers.
+#Ga = [0.0, 0.0, 0.0, 0.0, 0.0, 0.39]
+
+# Reaction energies.
+#dG = [-1.92, -2.09, -1.29, -2.33, -3.48, -0.46]
+
+Ga, dG = [], []
+
+# CO_g + *_t -> CO_t
+Ga.append(0.0)
+dG.append(-1.58)
+
+# CO_g + *_b -> CO_b
+Ga.append(0.0)
+dG.append(-1.68)
+
