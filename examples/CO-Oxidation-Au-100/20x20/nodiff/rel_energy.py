fa95f69ec68ed0c705d35e21d5e6ccc4147f5168
PytLab <shaozhengjiang@gmail.com>
2016-07-18 23:17:38 +0800

    Rename dir.

--- examples/CO-Oxidation-Au-100/20x20/nodiff/rel_energy.py
+++ examples/CO-Oxidation-Au-100/20x20/nodiff/rel_energy.py
@@ -16,19 +16,19 @@ dG.append(-0.54)
 
 # O2_g + 2*_b -> O2_2b
 Ga.append(0.0)
-dG.append(-1.02)
+dG.append(0.07)
 
 # O2_g + 2*_t -> O2_2t
 Ga.append(0.0)
-dG.append(-0.71)
+dG.append(-0.01)
 
 # O2_2b <-> O-O_2b -> 2O_b
-Ga.append(0.48)
-dG.append(0.47)
+Ga.append(0.46)
+dG.append(0.43)
 
 # O2_2b + CO_b <-> O-OCO_3b -> O_b + CO2_g + 2*_b
-Ga.append(0.42)
-dG.append(-0.52)
+Ga.append(0.46)
+dG.append(-2.57)
 
 # O_b + CO_t -> CO2_g + *_b + *_t
 Ga.append(0.0)
@@ -53,3 +53,4 @@ dG.append(-2.05)
 # O_b + *_t <-> O_t + *_b -> O_b + *_t
 Ga.append(0.91)
 dG.append(0.0)
+
