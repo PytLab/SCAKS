fcf2a49073dd24cb9da104c811d2d28d4964d7ee
PytLab <shaozhengjiang@gmail.com>
2016-07-19 17:10:43 +0800

    Add elements to CoordsGroup class.

--- kynetix/utilities/coordinate_utilities.py
+++ kynetix/utilities/coordinate_utilities.py
@@ -4,38 +4,104 @@ Module for doing coordinates group operation.
 
 import copy
 
+import numpy as np
+
 
 class CoordsGroup(object):
 
     # Static class varibles.
     tolerance = 1e-4
 
-    def __init__(self, coordinates=None):
+    def __init__(self, coordinates=None, elements=None):
+        """
+        Constructor.
+
+        Parameters:
+        -----------
+        coordinates: A list of coordinates, default value is [].
+
+        elements: A list of elements, list of str,
+                  default value is ["V", ...].
+        """
         if coordinates is None:
             self.__coords = []
         else:
             self.__coords = coordinates
 
-    def append(self, coordinate):
+        if elements is None:
+            self.__elements = ["V"]*len(self.__coords)
+        else:
+            self.__elements = elements
+
+        # Check.
+        if len(self.__coords) != len(self.__elements):
+            msg = "Length of coordinates({}) and elements({}) are different."
+            msg = msg.format(len(self.__coords), len(self.__elements))
+            raise ValueError(msg)
+
+    def __check_coordinate(self, coordinate):
+        """
+        Private helper function to check validty of coordinate.
+        """
+        msg = "coordinate must be a list of three float numbers."
+        if type(coordinate) is not list or len(coordinate) != 3:
+            raise ValueError(msg)
+
+        for entry in coordinate:
+            if type(entry) is not float:
+                raise ValueError(msg)
+
+        return coordinate
+
+    def append(self, coordinate, element=None):
         """
         Function to append a new coordinate.
-        """
-        # Check the parameter.
-        if type(coordinate) is not list and len(coordinate) != 3:
-            raise ValueError("coordinate must be a list of three float numbers.")
 
+        Parameters:
+        -----------
+        coordinate: A list of 3 float.
+
+        element: str, default value is "V".
+        """
+        # Append coordinates.
+        self.__check_coordinate(coordinate)
         self.__coords.append(coordinate)
 
-    def extend(self, coordinates):
+        # Append elements.
+        if element is None:
+            self.__elements.append("V")
+        else:
+            if type(element) is not str:
+                raise ValueError("element must be a string.")
+            self.__elements.append(element)
+
+    def extend(self, coordinates, elements=None):
         """
         Function to add new coordinates.
+
+        Parameters:
+        -----------
+        coordinates: A list of coordinates.
+
+        elements: A list of elements, list of str, default value is ["V", ...].
         """
-        for coordinate in coordinates:
-            self.append(coordinate)
+        if elements is None:
+            elements = ["V"]*len(coordinates)
+
+        for coordinate, element in zip(coordinates, elements):
+            self.append(coordinate, element)
 
     def move(self, move_vector):
         """
         Function to move all coordinates.
+
+        Parameters:
+        -----------
+        move_vector: Move vector, list of 3 floats.
+
+        Returns:
+        --------
+        A new moved CoordsGroup object.
         """
         # Check the parameter.
         if type(move_vector) is not list and len(move_vector) != 3:
@@ -45,9 +111,7 @@ class CoordsGroup(object):
         ori_coords = np.array(self.__coords)
         new_coords = (ori_coords + move_vector).tolist()
 
-        self.__coords = new_coords
-
-        return new_coord
+        return CoordsGroup(new_coords, self.__elements)
 
     @staticmethod
     def __compare_coords(coord1, coord2):
@@ -59,34 +123,39 @@ class CoordsGroup(object):
                 return False
         return True
 
-    @staticmethod
-    def __merge_coordinates(coords1, coords2):
-        """
-        Private helper function to merge tow coordinates groups.
-        """
-        merged_coords = copy.copy(coords1)
-
-        for coord in coords2:
-            in_it = False
-            for ref_coord in coords1:
-                coords_equal = CoordsGroup.__compare_coords(coord, ref_coord)
-                if coords_equal:
-                    in_it = True
-                    break
-            if not in_it:
-                merged_coords.append(coord)
-
-        return merged_coords
-
     def __add__(self, another):
         """
         Operator `+` overload function.
         """
-        merged_coords = self.__merge_coordinates(self.__coords,
-                                                 another.coordinates())
-        coords_group = CoordsGroup(merged_coords)
+        coords1, coords2 = self.__coords, another.coordinates()
+        elems1, elems2 = self.__elements, another.elements()
 
-        return coords_group
+        merged_coords = copy.copy(coords1)
+        merged_elems = copy.copy(elems1)
+
+        # Loop to add different entries.
+        for coord, elem in zip(coords2, elems2):
+            # Loop to check if the coord is in the first coords.
+            in_it = False
+            for idx, (ref_coord, ref_elem) in enumerate(zip(coords1, elems1)):
+                coords_equal = CoordsGroup.__compare_coords(coord, ref_coord)
+
+                if coords_equal:
+                    # Check element, replace or not.
+                    if elem != ref_elem:
+                        if ref_elem == "V":
+                            merged_elems[idx] = elem
+
+                    in_it = True
+                    break
+
+            if not in_it:
+                merged_coords.append(coord)
+                merged_elems.append(elem)
+
+        merged_coords_group = CoordsGroup(merged_coords, merged_elems)
+
+        return merged_coords_group
 
     def __len__(self):
         return len(self.__coords)
@@ -96,4 +165,10 @@ class CoordsGroup(object):
         Query function to get coordinates.
         """
         return self.__coords
+
+    def elements(self):
+        """
+        Query function for elements.
+        """
+        return self.__elements
 
--- unit_test/kynetix_test/utilities/coordinates_utilities_test.py
+++ unit_test/kynetix_test/utilities/coordinates_utilities_test.py
@@ -12,7 +12,6 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
         # Test case setting.
         self.maxDiff = None
 
-
     def test_construction(self):
         " Test CoordsGroup object can be constructed correctly. "
         fcc_coords = [[0.0, 0.0, 0.0],
@@ -26,6 +25,14 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
         fcc = CoordsGroup(fcc_coords)
 
         self.assertListEqual(fcc_coords, fcc.coordinates())
+        self.assertEqual(7, len(fcc))
+
+        # Check with elements.
+        elements = ['V', 'V', 'V', 'V', 'V', 'V', 'C']
+        fcc = CoordsGroup(fcc_coords, elements)
+
+        self.assertListEqual(fcc_coords, fcc.coordinates())
+        self.assertListEqual(elements, fcc.elements())
         self.assertEqual(7, len(fcc))
 
     def test_append(self):
@@ -51,6 +58,34 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
                       [0.0, 0.7, 0.7]]
         ret_coords = fcc.coordinates()
         self.assertListEqual(ref_coords, ret_coords)
+
+    def test_append_with_element(self):
+        # Check with element.
+        fcc_coords = [[0.0, 0.0, 0.0],
+                      [1.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.0, 0.5, 0.0],
+                      [0.5, 0.0, 0.0],
+                      [0.5, 0.5, 0.0],
+                      [0.3333333333333333, 0.3333333333333333, 0.0]]
+
+        fcc = CoordsGroup(fcc_coords)
+        fcc.append([0.0, 0.7, 0.7], "C")
+
+        ret_coords = fcc.coordinates()
+        ref_coords = [[0.0, 0.0, 0.0],
+                      [1.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.0, 0.5, 0.0],
+                      [0.5, 0.0, 0.0],
+                      [0.5, 0.5, 0.0],
+                      [0.3333333333333333, 0.3333333333333333, 0.0],
+                      [0.0, 0.7, 0.7]]
+        self.assertListEqual(ref_coords, ret_coords)
+
+        ref_elements = ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'C']
+        ret_elements = fcc.elements()
+        self.assertListEqual(ref_elements, ret_elements)
 
     def test_extend(self):
         " Test function extend() can work correctly. "
@@ -78,6 +113,35 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
         ret_coords = fcc.coordinates()
 
         self.assertListEqual(ref_coords, ret_coords)
+
+    def test_extend_with_elements(self):
+        " Test function extend() can work correctly with elements. "
+        fcc_coords = [[0.0, 0.0, 0.0],
+                      [1.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.0, 0.5, 0.0],
+                      [0.5, 0.0, 0.0],
+                      [0.5, 0.5, 0.0],
+                      [0.3333333333333333, 0.3333333333333333, 0.0]]
+
+        fcc = CoordsGroup(fcc_coords)
+        fcc.extend([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]], ["C", "C"])
+
+        ref_coords = [[0.0, 0.0, 0.0],
+                      [1.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.0, 0.5, 0.0],
+                      [0.5, 0.0, 0.0],
+                      [0.5, 0.5, 0.0],
+                      [0.3333333333333333, 0.3333333333333333, 0.0],
+                      [1.0, 1.0, 1.0],
+                      [1.0, 1.0, 1.0]]
+        ret_coords = fcc.coordinates()
+        self.assertListEqual(ref_coords, ret_coords)
+
+        ref_elements = ['V', 'V', 'V', 'V', 'V', 'V', 'V', 'C', 'C'] 
+        ret_elements = fcc.elements()
+        self.assertListEqual(ref_elements, ret_elements)
 
     def test_move(self):
         " Make sure coordinates can move correctly."
@@ -111,7 +175,9 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
                       [0.5, 0.0, 0.0],
                       [0.5, 0.5, 0.0],
                       [0.3333333333333333, 0.3333333333333333, 0.0]]
-        fcc = CoordsGroup(fcc_coords)
+        fcc_elems = ['V', 'V', 'V', 'V', 'V', 'V', 'C']
+
+        fcc = CoordsGroup(fcc_coords, fcc_elems)
 
         top_coords = [[0.0, 0.0, 0.0],
                       [0.0, 0.5, 0.0],
@@ -126,16 +192,14 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
                       [-0.6666666666666666, 0.3333333333333333, 0.0],
                       [-0.5, 0.5, 0.0],
                       [-0.3333333333333333, 0.6666666666666666, 0.0]]
-        top = CoordsGroup(top_coords)
+        top_elems = ['C', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V']
+        top = CoordsGroup(top_coords, top_elems)
 
-        merge = fcc + top
+        merge = top + fcc
         ref_coords = [[0.0, 0.0, 0.0],
-                      [1.0, 0.0, 0.0],
-                      [0.0, 1.0, 0.0],
                       [0.0, 0.5, 0.0],
-                      [0.5, 0.0, 0.0],
-                      [0.5, 0.5, 0.0],
                       [0.3333333333333333, 0.3333333333333333, 0.0],
+                      [0.5, 0.0, 0.0],
                       [0.6666666666666666, -0.3333333333333333, 0.0],
                       [0.5, -0.5, 0.0],
                       [0.3333333333333333, -0.6666666666666666, 0.0],
@@ -144,9 +208,16 @@ class CoordinatesUtilitiesTest(unittest.TestCase):
                       [-0.5, 0.0, 0.0],
                       [-0.6666666666666666, 0.3333333333333333, 0.0],
                       [-0.5, 0.5, 0.0],
-                      [-0.3333333333333333, 0.6666666666666666, 0.0]]
+                      [-0.3333333333333333, 0.6666666666666666, 0.0],
+                      [1.0, 0.0, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.5, 0.5, 0.0]]
         ret_coords = merge.coordinates()
         self.assertListEqual(ref_coords, ret_coords)
+
+        ref_elems = ['C', 'V', 'C', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V', 'V']
+        ret_elems = merge.elements()
+        self.assertListEqual(ref_elems, ret_elems)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(CheckUtilitiesTest)
