e255814bbec5306bb878f475ea60b5c6d91e7837
PytLab <shaozhengjiang@gmail.com>
2016-08-28 20:49:44 +0800

    Use Agg as backend of matplotlib.

--- scripts/kmc_plot/plot_scatters_large-111.py
+++ scripts/kmc_plot/plot_scatters_large-111.py
@@ -4,6 +4,8 @@ logging.basicConfig(level=logging.INFO)
 import argparse
 
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 try:
     from PIL import Image
