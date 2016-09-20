c6a4a8b3216dd25b9f61351c997ad8c6fd907f1d
PytLab <shaozhengjiang@gmail.com>
2016-09-20 14:53:11 +0800

    Remove flush() function.

--- kynetix/solvers/kmc_plugins/frequency_analysis.py
+++ kynetix/solvers/kmc_plugins/frequency_analysis.py
@@ -75,20 +75,13 @@ class FrequencyAnalysis(KMCAnalysisPlugin):
 
     def setup(self, step, time, configuration, interactions):
         # Create statistic data file.
-        variables_str = ("times = []\nsteps = []\npicked_indices = []\n" +
+        variables_str = ("times = []\npicked_indices = []\n" +
                          "process_occurencies = []\n")
         with open(self.__filename, "w") as f:
             content = file_header + variables_str
             f.write(content)
 
     def registerStep(self, step, time, configuration, interactions):
-        # Collect processes occurencies.
-        self.__collect_occurencies(step, time, interactions)
-
-    def __collect_occurencies(self, step, time, interactions):
-        """
-        Private function to collect process occurencies.
-        """
         # Append picked index.
         picked_index = interactions.pickedIndex()
         self.__picked_indices.append(picked_index)
@@ -155,44 +148,5 @@ class FrequencyAnalysis(KMCAnalysisPlugin):
             # Info output.
             msg = "frequency informations are written to {}".format(self.__filename)
             self.__logger.info(msg)
-
-    def __flush(self):
-        """
-        Private function to flush data in buffer.
-        """
-        # Get picked index extension strings.
-        var_name = "picked_indices_{}".format(self.__flush_counter)
-        list_str = get_list_string(var_name, self.__picked_indices, ncols=40)
-        extend_str = "picked_indices.extend({})\n\n".format(var_name)
-        picked_indices_str = list_str + extend_str
-
-        # Get times extension strings.
-        var_name = "times_{}".format(self.__flush_counter)
-        list_str = get_list_string(var_name, self.__times)
-        extend_str = "times.extend({})\n\n".format(var_name)
-        times_str = list_str + extend_str
-
-        # Get steps extension strings.
-        var_name = "steps_{}".format(self.__flush_counter)
-        list_str = get_list_string(var_name, self.__steps, ncols=10)
-        extend_str = "steps.extend({})\n\n".format(var_name)
-        steps_str = list_str + extend_str
-
-        # Get all content to be flushed.
-        comment = ("\n# -------------------- flush {} " +
-                   "---------------------\n").format(self.__flush_counter)
-        content = comment + times_str + steps_str + picked_indices_str
-
-        # Flush to file.
-        with open(self.__filename, "a") as f:
-            f.write(content)
-
-        # Free buffers.
-        self.__picked_indices = []
-        self.__steps = []
-        self.__times = []
-
-        self.__flush_counter += 1
-
     # }}}
 
