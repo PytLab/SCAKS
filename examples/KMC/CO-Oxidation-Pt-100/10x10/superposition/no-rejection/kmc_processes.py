bc272527c1f2b151919b01b31fe4cd771380b828
pytlab <shaozhengjiang@gmail.com>
2016-11-18 22:25:04 +0800

    Add new examples.

---
+++ examples/KMC/CO-Oxidation-Pt-100/10x10/superposition/no-rejection/kmc_processes.py
@@ -0,0 +1,20 @@
+processes = []
+
+from scripts.kmc_processes_old import processes as sub_processes
+processes.extend(sub_processes)
+
+from scripts.input_1_out import processes as sub_processes
+processes.extend(sub_processes)
+
+from scripts.input_2_out import processes as sub_processes
+processes.extend(sub_processes)
+
+from scripts.input_3_out import processes as sub_processes
+processes.extend(sub_processes)
+
+from scripts.input_4_out import processes as sub_processes
+processes.extend(sub_processes)
+
+from scripts.input_5_out import processes as sub_processes
+processes.extend(sub_processes)
+
