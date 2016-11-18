dc5f2b37dfa9f2419fe84c0df9560b362079d042
pytlab <shaozhengjiang@gmail.com>
2016-11-18 09:20:05 +0800

    Use master process to measure time.

--- scripts/kmc_run/run.py
+++ scripts/kmc_run/run.py
@@ -30,7 +30,9 @@ if "__main__" == __name__:
     parser = model.parser()
     parser.parse_data(filename="rel_energy.py", relative=True)
 
-    start = time.time()
+    if mpi_master:
+        start = time.time()
+
     try:
         model.run_kmc()
     except Exception as e:
@@ -40,10 +42,9 @@ if "__main__" == __name__:
             logger.exception(msg)
         raise e
 
-    end = time.time()
-    t = end - start
-    h, m, s = convert_time(t)
-
     if mpi_master:
+        end = time.time()
+        t = end - start
+        h, m, s = convert_time(t)
         logger.info("Time used: {:d} h {:d} min {:f} sec".format(h, m, s))
 
