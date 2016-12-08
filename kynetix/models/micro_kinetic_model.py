9f4fc6ea781b08cbab4b2926ac92b1c084bac362
pytlab <shaozhengjiang@gmail.com>
2016-12-08 22:49:24 +0800

    Revert "Remove archived_variables property."
    This reverts commit b9746824477c7aaa932e9b9245b2e9edfceb8306.

--- kynetix/models/micro_kinetic_model.py
+++ kynetix/models/micro_kinetic_model.py
@@ -22,6 +22,11 @@ class MicroKineticModel(km.KineticModel):
                                        default="right",
                                        candidates=["right", "left"])
 
+    # Archived variables.
+    archived_variables = dc.Sequence("archive_data",
+                                     default=["steady_state_coverages"],
+                                     entry_type=str)
+
     # Numerical representation.
     numerical_representation = dc.String("numerical_representation",
                                          default="mpmath",
