55786b59ee8297f2daa32f2500af59c770949312
pytlab <shaozhengjiang@gmail.com>
2016-12-12 16:28:22 +0800

    Fix bug in __get__() of descriptor.Property.
    The returned value could be a protected not a private variable.

--- kynetix/descriptors/descriptors.py
+++ kynetix/descriptors/descriptors.py
@@ -202,12 +202,7 @@ class Property(object):
         self.func = func
 
     def __get__(self, instance, cls):
-        val = self.func(instance)
-        attr_name = "_{}__{}".format(instance.__class__.__name__,
-                                     self.func.__name__)
-        if attr_name not in instance.__dict__:
-            setattr(instance, attr_name, val)
-        return val
+        return self.func(instance)
 
     def __set__(self, instance, value):
         msg ="Changing value of {}.{} is not allowed".format(instance.__class__.__name__,
