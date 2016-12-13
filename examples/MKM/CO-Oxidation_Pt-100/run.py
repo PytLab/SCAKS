9219015cd38801faa4d71029c35745943b5824ca
pytlab <shaozhengjiang@gmail.com>
2016-12-13 15:47:28 +0800

    Remove energy correction.

--- examples/MKM/CO-Oxidation_Pt-100/run.py
+++ examples/MKM/CO-Oxidation_Pt-100/run.py
@@ -13,8 +13,8 @@ from kynetix.utilities.format_utilities import convert_time
 
 # Custom parameters.
 UseRelativeEnergy = True    # Use only relative energies.
-OdeInterval = 0.01          # ODE integration time interval.
-OdeEnd = 100          # ODE integration time limit.
+OdeInterval = 0.0001          # ODE integration time interval.
+OdeEnd = 10          # ODE integration time limit.
 OdeOutput = False           # Output ODE integration data or not.
 CalcXRC = False             # Calculate Degree of Rate Control(XRC) or not.
 ProductionName = "CO2_g"  # Production name of your model.
@@ -45,7 +45,6 @@ if "__main__" == __name__:
         solver = model.solver
         parser.parse_data(relative=UseRelativeEnergy)
         solver.get_data()
-        solver.correct_relative_energies(method="entropy")
 
         # Initial coverages guess.
         trajectory = solver.solve_ode(time_span=OdeInterval,
