1510baab4a5a0d32bb41c6f65b4bda00eab6aea5
pytlab <shaozhengjiang@gmail.com>
2016-12-16 10:57:49 +0800

    Add new more mkm plot scripts.

---
+++ scripts/mkm_plot/plot_cvgs_tof.py
@@ -0,0 +1,75 @@
+import commands
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if "__main__" == __name__:
+    # Plot TOFs.
+    globs, locs = {}, {}
+    execfile("auto_tofs.py", globs, locs)
+    pressures = locs['pCO']
+    TONs = locs['tofs']
+
+    fig = plt.figure()
+
+    # Plot cvgs.
+    globs, locs = {}, {}
+    execfile("auto_cvgs.py", globs, locs)
+    coverages = locs['cvgs']
+    adsorbates = locs['adsorbates']
+
+
+    ax1 = fig.add_subplot(111)
+    pressure_range = max(pressures) - min(pressures)
+    ax1.set_ylim(-0.1, 1.1)
+    ax1.set_xlim(min(pressures)-pressure_range*0.1, max(pressures)+pressure_range*0.1)
+    ax1.set_xlabel(r"$\bf{P(CO_g)/bar}$")
+    ax1.set_ylabel(r"$\bf{Coverages}$")
+
+
+    coverages = zip(*coverages)
+    CO_cvgs = np.array(coverages[0]) + np.array(coverages[1])
+    O_cvgs = np.array(coverages[2])
+    cvgs_dict = {"CO": CO_cvgs, "O": O_cvgs}
+    colors = ["#CD6889", "#7D9EC0"]
+    for idx, sp in enumerate(cvgs_dict):
+        ax1.plot(pressures, cvgs_dict[sp], color=colors[idx],
+                                           linewidth=2.0,
+                                           marker='o',
+                                           markerfacecolor=colors[idx],
+                                           markeredgecolor=colors[idx],
+                                           markersize=7.0,
+                                           alpha=0.7,
+                                           label=sp)
+
+    # Plot TOF.
+    ax2 = ax1.twinx()
+    pressure_range = max(pressures) - min(pressures)
+    TON_range = max(TONs) - min(TONs)
+    ax2.set_ylim(min(TONs)-TON_range*0.1, max(TONs)+TON_range*0.1)
+    ax2.set_ylabel(r"$\bf{TOF/s^-1}$")
+    ax2.plot(pressures, TONs, color="#0F0F0F",
+                              linewidth=2.0,
+                              marker='o',
+                              markerfacecolor="#0F0F0F",
+                              markeredgecolor="#0F0F0F",
+                              markersize=7.0,
+                              alpha=0.3,
+                              label='TOF')
+
+    # Trick for labels.
+    ax1.plot(0, 0, color="#0F0F0F",
+                   linewidth=2.0,
+                   marker='o',
+                   markerfacecolor="#0F0F0F",
+                   markeredgecolor="#0F0F0F",
+                   markersize=7.0,
+                   alpha=0.3,
+                   label='TOF')
+    ax1.grid(True)
+    ax1.legend(loc=0)
+
+    plt.show()
+
