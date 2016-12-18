51d94adbbc211f6452ac54bf3e5bbbab196418a3
pytlab <shaozhengjiang@gmail.com>
2016-12-18 22:15:50 +0800

    Add abs_energy.py

---
+++ unit_test/mkm_inputs/abs_energy.py
@@ -0,0 +1,19 @@
+# Absolute energies for all species.
+absolute_energies = {
+
+    'CO2_g': 0.0 # eV
+
+    'CO_g': 0.0 # eV
+
+    'O2_g': 0.0 # eV
+
+    'CO_s': 0.0 # eV
+
+    'O_s': 0.0 # eV
+
+    'CO-O_2s': 0.0 # eV
+
+    '*_s': 0.0 # eV
+
+}
+
