da910b05033b85b4e62e442d5737154cd7dcded1
pytlab <shaozhengjiang@gmail.com>
2016-12-20 14:01:34 +0800

    Add Memoize property.

--- kynetix/solvers/steady_state_solver.py
+++ kynetix/solvers/steady_state_solver.py
@@ -18,6 +18,24 @@ from kynetix.solvers.rootfinding_iterators import *
 from kynetix.solvers.mean_field_solver import MeanFieldSolver
 
 
+class Memoized(object):
+    def __init__(self, func):
+        self.func = func
+        self.results = {}
+
+    def __get__(self, instance, cls):
+        self.instance = instance
+        return self
+
+    def __call__(self, *args):
+        key = args
+        try:
+            return self.results[key]
+        except KeyError:
+            self.results[key] = self.func(self.instance, *args)
+            return self.results[key]
+
+
 class SteadyStateSolver(MeanFieldSolver):
     def __init__(self, owner):
         # {{{
@@ -390,6 +408,7 @@ class SteadyStateSolver(MeanFieldSolver):
         return derivation_expression
         # }}}
 
+    @Memoized
     def poly_adsorbate_derivation(self, adsorbate_name, poly_expression):
         """
         Expect a polynomial expression of dtheta_dt and an adsorbate_name,
