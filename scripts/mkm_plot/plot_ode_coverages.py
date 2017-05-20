076763be8353571b498086e396f755dbf640ca36
pytlab <shaozhengjiang@gmail.com>
2017-05-20 19:34:32 +0800

    Modify ode coverages script.

--- scripts/mkm_plot/plot_ode_coverages.py
+++ scripts/mkm_plot/plot_ode_coverages.py
@@ -22,23 +22,28 @@ if adsorbate_names and len(adsorbate_names) != len(coverages[0]):
     sys.exit(1)
 
 # plot
-plt.subplot(111)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
 coverages = np.array(coverages)
-if adsorbate_names:
-    for idx in range(len(adsorbate_names)):
-        ads = r'$\bf{' + adsorbate_names[idx] + r'^{*}}$'
-        cvgs = coverages[:, idx]
-        plt.plot(times, cvgs, label=ads, linewidth=2.5)
-        plt.legend()
-else:
-    for idx in range(len(coverages[0])):
-        cvgs = coverages[:, idx]
-        plt.plot(times, cvgs, linewidth=2.5)
+CO_cvgs = coverages[:, 0]
+O_cvgs = coverages[:, 1]
+Vac_cvgs = np.ones(len(coverages)) - CO_cvgs - O_cvgs
 
-plt.ylabel(r'$\bf{Coverages}$', fontsize=16)
-plt.xlabel(r'$\bf{Time (s)}$', fontsize=16)
-plt.ylim(-0.1, 1.1)
-plt.grid(True)
+ax.set_ylim(-0.1, 1.1)
+time = times[-1]
+left, right = -time*0.05, time*1.05
+ax.set_xlim(left, right)
 
+label = r'$\bf{CO^{*}}$'
+ax.scatter(times, CO_cvgs, alpha=0.6, s=150.0, edgecolor="#000000", facecolor="#000000", label=label)
+label = r'$\bf{O^{*}}$'
+ax.scatter(times, O_cvgs, alpha=0.6, s=150.0, edgecolor="#E41615", facecolor="#E41615", label=label)
+label = r'$\bf{*}$'
+ax.scatter(times, Vac_cvgs, alpha=0.6, s=150.0, edgecolor="#0F387A", facecolor="#0F387A", label=label)
+ax.plot([left, right], [1.916e-01, 1.916e-01], linewidth=1, color="#AEAEAE")
+ax.plot([left, right], [3.322e-02, 3.322e-02], linewidth=1, color="#AEAEAE")
+
+plt.legend()
 plt.show()
 
