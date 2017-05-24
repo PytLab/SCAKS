0c86a29c276119abc0d65217ec28a6bcf7c6c15e
pytlab <shaozhengjiang@gmail.com>
2017-05-24 22:19:37 +0800

    Update scripts for 1d plotting.

--- scripts/kmc_plot/batch_plot/plot_cvgs.py
+++ scripts/kmc_plot/batch_plot/plot_cvgs.py
@@ -1,54 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import commands
-import os
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-species_names = {"O": ("O_s", "O_r", "O_u", "O_d", "O_l"),
-                 "CO": ("C")}
-colors = ["#CD6889", "#7D9EC0"]
+with np.load("cvgs-data.npz") as data:
+    pressures = data["p"]
+    CO_cvgs = data['CO_cvgs']
+    O_cvgs = data['O_cvgs']
 
-if "__main__" == __name__:
-    status, output = commands.getstatusoutput("ls")
-    dirs = [d for d in output.split("\n") if d.startswith("0.")]
+# Plot.
+fig = plt.figure()
+ax = fig.add_subplot(111)
+pressure_range = max(pressures) - min(pressures)
+ax.set_xlim(min(pressures)-pressure_range*0.1, max(pressures)+pressure_range*0.1)
+ax.set_ylim(-0.1, 1.1)
+ax.set_xlabel(r"$P_{CO_{(g)}}/bar$")
+ax.set_ylabel(r"$Coverages$")
 
-    pressures = []
-    coverages = {"O": [], "CO": []}
+# Plot CO cvgs.
+ax.plot(pressures, CO_cvgs, linewidth=2.0,
+                            color="#CD6889",
+                            marker='o',
+                            markerfacecolor="#CD6889",
+                            markeredgecolor="#CD6889",
+                            markersize=7.0,
+                            label=r"$\theta_{CO^{*}}$")
 
-    for d in dirs:
-        filename = "{}/auto_coverages.py".format(d)
-        if os.path.exists(filename):
-            globs, locs = {}, {}
-            exec(open(filename, "rb").read(), globs, locs)
-
-            possible_types = locs["possible_types"]
-            for species, type_names in species_names.items():
-                coverage = 0.0
-                for type_name in type_names:
-                    index = possible_types.index(type_name)
-                    coverage += locs["coverages"][index][-1]
-                coverages[species].append(coverage)
-            pressures.append(float(d))
-
-    # Plot.
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    pressure_range = max(pressures) - min(pressures)
-    ax.set_xlim(min(pressures)-pressure_range*0.1, max(pressures)+pressure_range*0.1)
-    ax.set_ylim(-0.1, 1.1)
-    ax.set_xlabel(r"$\bf{P(CO_g)/bar}$")
-    ax.set_ylabel(r"$\bf{Coverages}$")
-    for idx, species in enumerate(coverages):
-        ax.plot(pressures, coverages[species], linewidth=2.0,
-                                               color=colors[idx],
-                                               marker='o',
-                                               markerfacecolor=colors[idx],
-                                               markeredgecolor=colors[idx],
-                                               markersize=7.0,
-                                               label=species)
-    ax.legend()  
-    ax.grid(True)
-    plt.show()
+# Plot O cvgs.
+ax.plot(pressures, O_cvgs, linewidth=2.0,
+                           color="#7D9EC0",
+                           marker='o',
+                           markerfacecolor="#7D9EC0",
+                           markeredgecolor="#7D9EC0",
+                           markersize=7.0,
+                           label=r"$\theta_{O^{*}}$")
+ax.legend()  
+plt.show()
 
--- scripts/kmc_plot/batch_plot/plot_cvgs_tof.py
+++ scripts/kmc_plot/batch_plot/plot_cvgs_tof.py
@@ -1,96 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import commands
-import os
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-if "__main__" == __name__:
-    status, output = commands.getstatusoutput("ls")
-    dirs = [d for d in output.split("\n") if d.startswith("0.")]
+with np.load("tofs-data.npz") as data:
+    pressures = data['p']
+    tofs = data['tofs']
 
-    # Plot cvgs.
-    species_names = {"O": ("O_s", "O_r", "O_u", "O_d", "O_l"),
-                     "CO": ("C")}
-    colors = ["#CD6889", "#7D9EC0"]
+with np.load("cvgs-data.npz") as data:
+    CO_cvgs = data['CO_cvgs']
+    O_cvgs = data['O_cvgs']
 
-    pressures = []
-    coverages = {"O": [], "CO": []}
+fig = plt.figure()
+ax = fig.add_subplot(111)
+pressure_range = max(pressures) - min(pressures)
+ax.set_xlim(min(pressures)-pressure_range*0.1, max(pressures)+pressure_range*0.1)
+ax.set_ylim(-0.1, 1.1)
+ax.set_xlabel(r"$P_{CO_{(g)}}/bar$")
+ax.set_ylabel(r"$Coverages$")
 
-    for d in dirs:
-        filename = "{}/auto_coverages.py".format(d)
-        if os.path.exists(filename):
-            globs, locs = {}, {}
-            exec(open(filename, "rb").read(), globs, locs)
+# Plot CO
+ax.plot(pressures, CO_cvgs, linewidth=2.0,
+                            color="#0F0F0F",
+                            marker='o',
+                            markerfacecolor="#0F0F0F",
+                            markeredgecolor="#0F0F0F",
+                            markersize=7.0,
+                            alpha=0.7,
+                            label=r"$\theta_{CO^{*}}$")
 
-            possible_types = locs["possible_types"]
-            for species, type_names in species_names.items():
-                coverage = 0.0
-                for type_name in type_names:
-                    index = possible_types.index(type_name)
-                    coverage += locs["coverages"][index][-1]
-                coverages[species].append(coverage)
-            pressures.append(float(d))
+# Plot O
+ax.plot(pressures, O_cvgs, linewidth=2.0,
+                           color="#CD6889",
+                           marker='o',
+                           markerfacecolor="#CD6889",
+                           markeredgecolor="#CD6889",
+                           markersize=7.0,
+                           alpha=0.7,
+                           label=r"$\theta_{O^{*}}$")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    pressure_range = max(pressures) - min(pressures)
-    ax.set_xlim(min(pressures)-pressure_range*0.1, max(pressures)+pressure_range*0.1)
-    ax.set_ylim(-0.1, 1.1)
-    ax.set_xlabel(r"$\bf{P(CO_g)/bar}$")
-    ax.set_ylabel(r"$\bf{Coverages}$")
-    for idx, species in enumerate(coverages):
-        ax.plot(pressures, coverages[species], linewidth=2.0,
-                                               color=colors[idx],
-                                               marker='o',
-                                               markerfacecolor=colors[idx],
-                                               markeredgecolor=colors[idx],
-                                               markersize=14.0,
-                                               label=species,
-                                               alpha=0.7)
+# Plot TOF.
+ax2 = ax.twinx()
+tof_range = max(tofs) - min(tofs)
+ax2.set_ylim(min(tofs)-tof_range*0.1, max(tofs)+tof_range*0.1)
+ax2.set_ylabel(r"$TOF/s^{-1}$")
+ax2.plot(pressures, tofs, color="#7D9EC0",
+                          linewidth=2.0,
+                          marker='s',
+                          markerfacecolor="#7D9EC0",
+                          markeredgecolor="#7D9EC0",
+                          markersize=7.0,
+                          alpha=0.3,
+                          label="$TOF$")
+# Trick for labels.
+ax.plot(0, 0, color="#0F0F0F",
+              linewidth=2.0,
+              marker='s',
+              markerfacecolor="#0F0F0F",
+              markeredgecolor="#0F0F0F",
+              markersize=7.0,
+              alpha=0.3,
+              label='$TOF$')
+ax.legend(loc=0)  
+#ax.grid(True)
 
-    # Plot TOF.
-    TONs = []
-    for d in dirs:
-        filename = "{}/auto_frequency.py".format(d)
-        if os.path.exists(filename):
-            globs, locs = {}, {}
-            exec(open(filename, "rb").read(), globs, locs)
-            try:
-                reaction_rates = locs["reaction_rates"]
-            except KeyError:
-                continue
-            TON = 0.0
-            reactions = sorted(reaction_rates.keys())
-            for idx in [0, 2, 8, 12]:
-                TON += reaction_rates[reactions[idx]]
-            TONs.append(TON)
-
-    # Plot
-    ax2 = ax.twinx()
-    TON_range = max(TONs) - min(TONs)
-    ax2.set_ylim(min(TONs)-TON_range*0.1, max(TONs)+TON_range*0.1)
-    ax2.set_ylabel(r"$\bf{TON/s^-1}$")
-    ax2.plot(pressures, TONs, color="#0F0F0F",
-                              linewidth=2.0,
-                              marker='^',
-                              markerfacecolor="#0F0F0F",
-                              markeredgecolor="#0F0F0F",
-                              markersize=14.0,
-                              alpha=0.3,
-                              label="TOF")
-    # Trick for labels.
-    ax.plot(0, 0, color="#0F0F0F",
-                  linewidth=2.0,
-                  marker='^',
-                  markerfacecolor="#0F0F0F",
-                  markeredgecolor="#0F0F0F",
-                  markersize=14.0,
-                  alpha=0.3,
-                  label='TOF')
-    ax.legend(loc=0)  
-    ax.grid(True)
-
-    plt.show()
+plt.show()
 
