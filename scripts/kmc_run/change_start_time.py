68b3d20eada8e31f5e48b98d98d0448c1898083c
pytlab <shaozhengjiang@gmail.com>
2017-05-24 22:24:28 +0800

    Add default start time setting.

--- scripts/kmc_run/change_start_time.py
+++ scripts/kmc_run/change_start_time.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from auto_lattice_trajectory_old import times
-
 globs, locs = {}, {}
 
 exec(open("auto_lattice_trajectory_old.py", "r").read(), globs, locs)
@@ -12,9 +10,14 @@ start_time = times[-1]
 with open("pt-100.mkm", "r") as f:
     content = f.readlines()
 
+found = False
 for idx, line in enumerate(content):
     if line.startswith("start_time"):
         content[idx] = "start_time = {}".format(start_time)
+        found = True
+
+if not found:
+    content.append('start_time = 0.0\n')
 
 with open("pt-100.mkm", "w") as f:
     f.writelines(content)
