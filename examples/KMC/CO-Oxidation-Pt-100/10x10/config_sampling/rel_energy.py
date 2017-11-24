23a7bee14fb6a738e19c82aee7b9b9f9dadad43f
pytlab <shaozhengjiang@gmail.com>
2017-11-24 16:35:53 +0800

    Add new example.

---
+++ examples/KMC/CO-Oxidation-Pt-100/10x10/config_sampling/rel_energy.py
@@ -0,0 +1,18 @@
+Ga, dG = [], []
+
+# O2_g + 2*_t -> O2_2t
+Ga.append(0.85)
+dG.append(-1.13)
+
+# O2_g + *_b -> O_b + O_b
+Ga.append(0.91)
+dG.append(-1.82)
+
+# O2_g + 2*_b -> 2O_b
+Ga.append(0.07)
+dG.append(-2.39)
+
+# O_b + *_t <-> O_t + *_b -> O_b + *_t
+Ga.append(1.15)
+dG.append(0.0)
+
---
+++ examples/KMC/CO-Oxidation-Pt-100/10x10/config_sampling/traj2poscar/traj2xyz.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from vaspy.atomco import PosCar
+
+def get_site_coordinates(sites, origin, h, unit_bases):
+    ''' 获取所有sites的在supercell中的分数坐标.
+
+    Parameters:
+    -----------
+    sites: 所有吸附位点的相对坐标
+    origin: 原点原子的分数坐标
+    h: 所有site高度
+    unit_bases: unitcell的基向量
+    '''
+    unit_bases = np.matrix(unit_bases).T
+    sites = np.matrix(sites).T
+    coords = unit_bases*sites
+    origin = np.matrix(origin).T
+    coords += origin
+    coords[-1, :] += h
+
+    # PDC.
+    coords = np.array(coords)
+    coords[coords > 1.0] -= 1.0
+
+    return coords.T.tolist()
+
+def get_config_poscar(config, positions, poscar_template='POSCAR'):
+    poscar = PosCar(poscar_template)
+    for atom, coord in zip(config, positions):
+        if atom.startswith('O'):
+            poscar.add_atom('O', coord)
+
+    return poscar
+
+if __name__ == '__main__':
+    from auto_lattice_trajectory import sites
+    origin = [0.201250, 0.158250, 0.370509]
+    h = 0.02
+    unit_bases = [[0.25, 0.0, 0.0],
+                  [0.0, 0.25, 0.0],
+                  [0.0, 0.0, 0.0]]
+    site_coords = get_site_coordinates(sites, origin, h, unit_bases)
+
+    from auto_lattice_trajectory import types
+    for i, config in enumerate(types):
+        poscar = get_config_poscar(config, site_coords)
+        filename = 'configs/config_{}.cif'.format(i)
+        with open(filename, 'w') as f:
+            f.write(poscar.get_cif_content())
+        print('{} created'.format(filename))
+
