f06a60fd44173324af04499297b40ec667ef04cb
pytlab <shaozhengjiang@gmail.com>
2017-11-27 15:00:51 +0800

    Update Pt(111) example.

--- examples/KMC/CO-Oxidation-Pt-111/20x20/run.py
+++ examples/KMC/CO-Oxidation-Pt-111/20x20/run.py
@@ -2,32 +2,32 @@
 Script to run kynetix kMC simulation.
 """
 
-import commands
 import logging
 import time
 
-from kynetix import mpi_master
-from kynetix.model import KineticModel
+from kynetix.mpicommons import mpi
+from kynetix.compatutil import subprocess
+from kynetix.models.kmc_model import KMCModel
 from kynetix.utilities.format_utilities import convert_time
 
 if "__main__" == __name__:
     # Remove old log file.
-    commands.getstatusoutput("rm -rm out.log auto_*")
+    subprocess.getstatusoutput("rm -rm out.log auto_*")
 
     # Set logger.
     logger = logging.getLogger("model.KMCModelRun")
 
     # Construct KMC model.
-    model = KineticModel(setup_file="pt-111.mkm")
-    parser = model.parser()
-    parser.parse_data(filename="rel_energy.py", relative=True)
+    model = KMCModel(setup_file="pt-111.mkm")
+    parser = model.parser
+    parser.parse_data(energy_file="rel_energy.py")
 
     start = time.time()
     try:
-        model.run_kmc()
+        model.run()
     except Exception as e:
         # Log exception info.
-        if mpi_master:
+        if mpi.is_master:
             msg = "{} exception is catched.".format(type(e).__name__)
             logger.exception(msg)
         raise e
@@ -36,6 +36,6 @@ if "__main__" == __name__:
     t = end - start
     h, m, s = convert_time(t)
 
-    if mpi_master:
+    if mpi.is_master:
         logger.info("Time used: {:d} h {:d} min {:f} sec".format(h, m, s))
 
