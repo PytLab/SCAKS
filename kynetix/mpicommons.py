b4a3a8c07becc6a543642bff01ff709d9c774a4e
pytlab <shaozhengjiang@gmail.com>
2018-06-26 22:17:17 +0800

    Add more functions in mpicommon.

--- kynetix/mpicommons.py
+++ kynetix/mpicommons.py
@@ -2,6 +2,10 @@
 Module for wrapped MPI interfaces used in Kynetix.
 """
 
+import logging
+from itertools import chain
+from functools import wraps
+
 try:
     from mpi4py import MPI
     MPI_INSTALLED = True
@@ -11,10 +15,18 @@ except ImportError:
 import kynetix.descriptors.descriptors as dc
 
 
-class MPIWrapper(object):
+class MPIUtil(object):
     def __init__(self):
-        # Nothing here.
-        pass
+        logger_name = 'kynetix.{}'.format(self.__class__.__name__)
+        self._logger = logging.getLogger(logger_name)
+
+    def bcast(self, data):
+        if MPI_INSTALLED:
+            mpi_comm = MPI.COMM_WORLD
+            bdata = mpi_comm.bcast(data, root=0)
+        else:
+            bdata = data
+        return bdata
 
     def barrier(self):
         if MPI_INSTALLED:
@@ -41,5 +53,59 @@ class MPIWrapper(object):
     def is_master(self):
         return self.rank == 0
 
-mpi = MPIWrapper()
+    # Utility methods.
+    def split_seq(self, sequence):
+        '''
+        Split the sequence according to rank and processor number.
+        '''
+        starts = [i for i in range(0, len(sequence), len(sequence)//self.size)]
+        ends = starts[1: ] + [len(sequence)]
+        start, end = list(zip(starts, ends))[self.rank]
+
+        return sequence[start: end]
+
+    def split_size(self, size):
+        '''
+        Split a size number(int) to sub-size number.
+        '''
+        if size < self.size:
+            warn_msg = ('Splitting size({}) is smaller than process ' +
+                        'number({}), more processor would be ' +
+                        'superflous').format(size, self.size)
+            self._logger.warning(warn_msg)
+            splited_sizes = [1]*size + [0]*(self.size - size)
+        elif size % self.size != 0:
+            residual = size % self.size
+            splited_sizes = [size // self.size]*self.size
+            for i in range(residual):
+                splited_sizes[i] += 1
+        else:
+            splited_sizes = [size // self.size]*self.size
+
+        return splited_sizes[self.rank]
+
+    def merge_seq(self, seq):
+        '''
+        Gather data in sub-process to root process.
+        '''
+        if self.size == 1:
+            return seq
+
+        mpi_comm = MPI.COMM_WORLD
+        merged_seq= mpi_comm.allgather(seq)
+        return list(chain(*merged_seq))
+
+mpi = MPIUtil()
+
+def master_only(func):
+    '''
+    Decorator to limit a function to be called
+    only in master process in MPI env.
+    '''
+    @wraps(func)
+    def _call_in_master_proc(*args, **kwargs):
+        if mpi.is_master:
+            return func(*args, **kwargs)
+
+    return _call_in_master_proc
 
