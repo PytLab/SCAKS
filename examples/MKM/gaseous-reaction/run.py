575e7aa184cf7d980c78024272496431bcfc906d
pytlab <shaozhengjiang@gmail.com>
2019-11-21 00:14:33 +0800

    Add reactions

--- examples/MKM/gaseous-reaction/run.py
+++ examples/MKM/gaseous-reaction/run.py
@@ -13,6 +13,23 @@ model_dict = {
         'O2_g + *_n -> O2_n',
         'O2_n + CH4_g + *_b <-> CH3-H_g + O2_n -> CH3_i + O_n + OH_b',
         'CH3_i + O_n <-> CH3-O_n -> OCH3_n',
+        'OCH3_n + OH_b + H_o <-> OCH3_n + OH-H_b + *_o -> OCH3_i + H2O_g + *_n + *_b',
+        'CH4_g + OH_b <-> CH3-H_g + OH_b -> CH3_i + H2O_g',
+        'OCH3_i <-> OCH2-H_i -> HCHO_g + H_i', #1
+        'OCH3_i + O2_g <-> OCH2-H_i + O2_g -> HCHO_g + OOH_i', #2
+        'HCHO_g + CH3_i <-> CH3_i + H-CHO_g -> CHO_i + CH4_g', #3
+        'CHO_i + O2_g <-> CO-H_i + O2_g -> CO_g + OOH_i', #4
+        'CHO_i <-> CO-H_i -> CO_g + H_i', #5
+        '2CH3_i -> C2H6_g', #6
+        '2H_i -> H2_g', #7
+        'H_i + CH4_g <-> CH3-H_g + H_i -> H2_g + CH3_i', #8
+        'H_i + C2H6_g <-> C2H5-H_g + H_i -> H2_g + C2H5_i', #9
+        'O2 + H_i <-> OO-H_i -> OOH_i', #10
+        'CH3_i + OOH_i -> CH3OOH_g', #11
+        'CH3OOH_g <-> CH3O-OH_g -> CH3O_i + OH_i', #12
+        'OH_i + H_i -> H2O_g', #13
+        'C2H6_g + CH3_i <-> C2H5-H_g + CH3_i -> C2H5_i + CH4_g', #14
+        'C2H5_i -> C2H4_g + H_i', #15
     ],
 
     'species_definitions': {
